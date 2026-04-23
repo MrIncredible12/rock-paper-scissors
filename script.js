87f07ab91efde516808e809e68a0be6d37f479fb
Jordan H <180706259+MrIncredible12@users.noreply.github.com>
2026-04-23 15:38:11 -0600

    Pseudocode created for getComputerChoice function.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // OUTPUT "Hello World" in web browser console to ensure connection to html file.
 console.log("Hello World");
+
+// DECLARE global variables.
+let compChoice;
+let userChoice;
+
+/*
+DECLARE getComputerChoice function.
+    GENERATE a random number between 0 and 1.
+    MULTIPLY the random number by 3.
+    ROUND DOWN the random number to the next integer.
+    ASSIGN number to compChoice.
+
+    IF compChoice is equal to 0 then:
+        ASSIGN "rock" to compChoice.
+    ELSE IF compChoice is equal to 1 then:
+        ASSIGN "paper" to compChoice.
+    ELSE (value is equal to 2) then:
+        ASSIGN "scissors" to compChoice.
+    END IF ELSE BLOCK.
+OUTPUT to console the computer choice to ensure desired outcomes.
+*/
